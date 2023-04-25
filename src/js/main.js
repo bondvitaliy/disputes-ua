d5ae581c7d5e1770d97b9e3982ab14d421741cda
Vitalii Bondarenko <bondvitaliy@gmail.com>
2023-04-25 20:43:46 +0300

    Added main files
    Added html + css files + main folders (fonts + images + js + sass)

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// let counter = 1;
+// function change_word(){
+//     // console.log(counter);
+//     $('[class*=text]').hide();
+//     $(`.text-${counter}`).show();
+//     counter = counter >= 5 ? 1: ++counter;
+// }
+
+// setInterval(change_word, 2000);
