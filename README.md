c37e56f3f3e6ce201da1ca65db2f5a225f327a71
Vitalii Bondarenko <bondvitaliy@gmail.com>
2023-04-30 01:41:41 +0300

    HTML partails - Contacts page
    +parsel.js installed
    +node.js installed
    +posthtml-include installed
    +contact page HTML include used
    +header + heading + contacts + footer HTML partials added

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # disputes-ua
 disputes.ua new design
 
-ISSUES
+CURRENT ISSUES
 
-1. ++ margin-padding head index - hero - (maybe remove the height) - !DONE!
+1. +++ margin-padding head index - hero - (maybe remove the height) - !DONE! - 
+
+1.1. maybe redo the CONTACTS + FORM above footer - resize the paddind -80px??? - !Ask Dmytro!
 
 2. +++ Installation of fonts - Lato Cyrillic - !DONE!
 
@@ -11,7 +13,7 @@ ISSUES
 
 4. contacts + form - remove the border-radius 15px and enlarge the size to the end of the page
 
-5. make a full-size map to the end of the page - WORK IN PROGRESS
+5. +++ make a full-size map to the end of the page - NOT NEEDED
 
 6. pop-up elements???
 
@@ -23,17 +25,17 @@ ISSUES
 
 10. language switcher - make button lang clickable
 
-11. chat form + replace png with svg
+11. chat button - replace png with svg + white dots indise (non-transparent)
 
-12. set the links to all  the buttons and internal links
+12. set the links to all the buttons and internal links
 
 13. make a list of the designer's issues
 
-14. create a GitHub project
+14. +++ create a GitHub project - !DONE!
 
 15. +++ Correct padding in the container - !DONE!
 
-16. False margins and paddings on all pages - WORK IN PROGRESS
+16. +++ False margins and paddings on all pages - !DONE!
 
 17. the drop-down menu on the services page
 
@@ -45,10 +47,26 @@ ISSUES
 
 21. +++ Turn rgba colors into hex in the variables - !DONE!
 
-22. remove NBSPs - "white-space: pre-line" instead
+22. +++ remove NBSPs - "white-space: pre-line" instead - !DONE!
 
 23. correct padding-margin on Service buttons
 
-24. add shadows on hanging elements - buttons, forms, etc
+24. add shadows on hanging elements - buttons, forms, etc - WORK IN PROGRESS - SHOW TO THE BOSS
 
 25. add cursor pointer on all buttons - WORK IN PROGRESS
+
+26. change the photo of boss (use overflow: hidden; on the img)
+
+// MAIN ISSUES
+
+1. do the ABOUT page
+
+2. do the buttons on SERVICE pages
+
+3. do the CHAT FORM
+
+4. do the LANGUAGE button
+
+5. do the PUBLICATIONS page to the end
+
+6. do the OPENED SERVICE page - !Ask Dmytro!
--- src/js/main.js
+++ src/js/main.js
@@ -70,3 +70,146 @@ setInterval(change_word, 2000);
 
 // setInterval(change_word, 2000);
 
+
+// HTML PARTIALS - W3SCHOOL
+
+// function includeHTML() {
+//     var z, i, elmnt, file, xhttp;
+//     /* Loop through a collection of all HTML elements: */
+//     z = document.getElementsByTagName("*");
+//     for (i = 0; i < z.length; i++) {
+//         elmnt = z[i];
+//         /*search for elements with a certain atrribute:*/
+//         file = elmnt.getAttribute("w3-include-html");
+//         if (file) {
+//             /* Make an HTTP request using the attribute value as the file name: */
+//             xhttp = new XMLHttpRequest();
+//             xhttp.onreadystatechange = function () {
+//                 if (this.readyState == 4) {
+//                     if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+//                     if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+//                     /* Remove the attribute, and call this function once more: */
+//                     elmnt.removeAttribute("w3-include-html");
+//                     includeHTML();
+//                 }
+//             }
+//             xhttp.open("GET", file, true);
+//             xhttp.send();
+//             /* Exit the function: */
+//             return;
+//         }
+//     }
+// }
+
+
+
+// AJAX INCLUDE FUNCTION
+
+// function includeHTML() {
+//     var element, file;
+//     /* Знаходимо всі HTML-елементи з атрибутом include */
+//     var elements = document.querySelectorAll('[include]');
+//     /* Проходимо по кожному з цих елементів */
+//     for (var i = 0; i < elements.length; i++) {
+//         element = elements[i];
+//         file = element.getAttribute('include');
+//         /* Завантажуємо вміст файлу за допомогою AJAX */
+//         var xhttp = new XMLHttpRequest();
+//         xhttp.onreadystatechange = function () {
+//             if (this.readyState == 4 && this.status == 200) {
+//                 /* Вставляємо вміст файлу в елемент */
+//                 element.innerHTML = this.responseText;
+//                 /* Викликаємо функцію includeHTML() знову, щоб обробити всі елементи */
+//                 includeHTML();
+//             }
+//         }
+//         xhttp.open('GET', file, true);
+//         xhttp.send();
+//         /* Вихід з функції, щоб AJAX міг завантажити вміст файлу */
+//         return;
+//     }
+// }
+
+// POSTHTML INCLUDE
+
+// const fs = require('fs');
+// const path = require('path');
+// const posthtml = require('posthtml');
+// const { parser } = require('posthtml-parser');
+// const { match } = require('posthtml/lib/api');
+// const expressions = require('posthtml-expressions');
+
+// module.exports = (options = {}) => {
+//     options.root = options.root || './';
+//     options.encoding = options.encoding || 'utf-8';
+
+//     return function posthtmlInclude(tree) {
+//         tree.parser = tree.parser || parser;
+//         tree.match = tree.match || match;
+
+//         tree.match({ tag: 'include' }, node => {
+//             let src = node.attrs.src || false;
+//             let content;
+//             let subtree;
+//             let source;
+//             let posthtmlExpressionsOptions = options.posthtmlExpressionsOptions || { locals: false };
+//             if (options.delimiters) {
+//                 posthtmlExpressionsOptions.delimiters = options.delimiters;
+//             }
+
+//             if (src) {
+//                 src = path.resolve(options.root, src);
+//                 source = fs.readFileSync(src, options.encoding);
+
+//                 try {
+//                     const localsRaw = node.attrs.locals || (node.content ? node.content.join().replace(/\n/g, '') : false);
+//                     const localsJson = JSON.parse(localsRaw);
+//                     posthtmlExpressionsOptions = {
+//                         ...posthtmlExpressionsOptions,
+//                         locals: posthtmlExpressionsOptions.locals ? Object.assign(posthtmlExpressionsOptions.locals, localsJson) : localsJson
+//                     };
+//                 } catch { }
+
+//                 if (posthtmlExpressionsOptions.locals) {
+//                     const result = posthtml()
+//                         .use(expressions(posthtmlExpressionsOptions))
+//                         .process(source, { sync: true });
+//                     source = result.html;
+//                 }
+
+//                 subtree = tree.parser(source);
+//                 subtree.match = tree.match;
+//                 subtree.parser = tree.parser;
+//                 subtree.messages = tree.messages;
+//                 content = source.includes('include') ? posthtmlInclude(subtree) : subtree;
+
+//                 if (tree.messages) {
+//                     tree.messages.push({
+//                         type: 'dependency',
+//                         file: src
+//                     });
+//                 }
+//             }
+
+//             return {
+//                 tag: false,
+//                 content
+//             };
+//         });
+
+//         return tree;
+//     };
+// };
+
+
+// PART 2
+const { readFileSync } = require('fs')
+
+const posthtml = require('posthtml')
+const include = require('posthtml-include')
+
+const html = readFileSync('index.html')
+
+posthtml([include({ encoding: 'utf8' })])
+    .process(html)
+    .then((result) => console.log(result.html))
